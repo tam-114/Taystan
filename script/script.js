bbb2b2e98cc78e3e7bde341ad726202105144483
tam-114 <t.stanley1412@gmail.com>
2023-08-11 03:46:14 -0400

    footer, privacy, terms and conditions

--- script/script.js
+++ script/script.js
@@ -6,3 +6,22 @@ function openHamburgerMenu(){
       x.className = "navBar";
   }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const formFields = document.querySelectorAll('.form-control');
+  const submitButton = document.getElementById('submitButton');
+
+  formFields.forEach(field => {
+    field.addEventListener('input', () => {
+      submitButton.style.backgroundColor = '#ffa037';
+      submitButton.style.transition = 'background-color 1s';
+    });
+
+    field.addEventListener('blur', () => {
+      const allFieldsEmpty = Array.from(formFields).every(field => field.value === '');
+      if (allFieldsEmpty) {
+        submitButton.style.backgroundColor = ''; // Reset to default or initial color
+      }
+    });
+  });
+});
